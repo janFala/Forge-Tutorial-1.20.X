6d5c1b72ae16d3d52a65778fec1c62c835aad303
studfalaja8725 <jan.falarowski@stud.th-rosenheim.de>
2023-10-27 22:25:31 +0200

    reset project and added own items scythe and spear

---
+++ src/main/java/net/rockeatersteve/tutorialmod/item/ModItems.java
@@ -0,0 +1,23 @@
+package net.rockeatersteve.tutorialmod.item;
+
+import net.rockeatersteve.tutorialmod.TutorialMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
+
+    public static final RegistryObject<Item> SCYTHE = ITEMS.register("scythe",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> SPEAR = ITEMS.register("spear",
+            () -> new Item(new Item.Properties()));
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
